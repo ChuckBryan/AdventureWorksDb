1986f35332440efff18da5ee6f0f90964df43e75
Chuck Bryan <cbryan@marathonus.com>
2025-05-22 16:46:30 -0400

    Remove deprecated scripts for building and running AdventureWorks SQL Server container

--- reset.ps1
+++
@@ -1,5 +0,0 @@
-# Reset wrapper script
-# Simply calls the script from the scripts folder
-
-$scriptPath = Join-Path $PSScriptRoot "scripts\reset.ps1"
-& $scriptPath
--- troubleshoot.ps1
+++
@@ -1,5 +0,0 @@
-# Troubleshoot wrapper script
-# Simply calls the script from the scripts folder
-
-$scriptPath = Join-Path $PSScriptRoot "scripts\troubleshoot.ps1"
-& $scriptPath
